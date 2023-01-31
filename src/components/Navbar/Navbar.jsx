99d8dd7979af09be69d2122ed9248c1d9a43d340
andras nemeth <andras.nemeth02@gmail.com>
2023-01-31 15:11:44 +0100

    Navbar-start

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlinveRestaurantMenu } from "react-icons/md";
 
+import images from "../../constants/images";
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app_navbar'>
+    <div className='app__navbar-logo'>
+      <img src={images.gericht} alt="app logo" />
+    </div>
+  </nav>
 );
 
 export default Navbar;
