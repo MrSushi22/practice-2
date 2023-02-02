2e5114ecdf2d5d7cb72fa8e5dd695e67428db40f
andras nemeth <andras.nemeth02@gmail.com>
2023-02-02 09:23:57 +0100

    Create the menu for the navbar, and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlinveRestaurantMenu } from "react-icons/md";
-
-import images from "../../constants/images";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
@@ -10,6 +9,13 @@ const Navbar = () => (
     <div className='app__navbar-logo'>
       <img src={images.gericht} alt="app logo" />
     </div>
+    <ul className="app__navbar-links">
+      <li className='p__opensans'><a href='#home'>Home</a></li>
+      <li className='p__opensans'><a href='#about'>About</a></li>
+      <li className='p__opensans'><a href='#menu'>Menu</a></li>
+      <li className='p__opensans'><a href='#awards'>Awards</a></li>
+      <li className='p__opensans'><a href='#contact'>Contact</a></li>
+    </ul>
   </nav>
 );
 
