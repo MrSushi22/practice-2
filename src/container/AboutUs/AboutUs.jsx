f2468781bb9d425cef280a4a4bf08e9f4b250ac7
andras nemeth <andras.nemeth02@gmail.com>
2023-02-08 11:54:19 +0100

    About us, History styled

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -12,7 +12,20 @@ const AboutUs = () => (
     <div className='app__aboutus-content flex__center'>
       <div className='app__aboutus-content_about'>
         <h1 className='headtext__cormorant'>About Us</h1>
-  
+        <img src={images.spoon} alt="abut_spoon" className='spoon__img' />
+        <p className='p__opensans'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, non ipsum mollitia dolores eius voluptates obcaecati at ullam totam quibusdam cupiditate eos, porro, et aspernatur ea quia optio facilis vero!</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt="abut_spoon" className='spoon__img' />
+        <p className='p__opensans'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, non ipsum mollitia dolores eius voluptates obcaecati at ullam totam quibusdam cupiditate eos, porro, et aspernatur ea quia optio facilis vero!</p>
+        <button type='button' className='custom__button'>Know More</button>
       </div>
 
     </div>
